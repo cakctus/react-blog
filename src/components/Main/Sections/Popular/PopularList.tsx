b453063e5a384180c52806572058ecf404c6794d
Cakctus <cakctusinc@gmail.com>
2022-10-29 14:10:40 +0300

    add scrollleft in block popular

--- src/components/Main/Sections/Popular/PopularList.tsx
+++ src/components/Main/Sections/Popular/PopularList.tsx
@@ -1,5 +1,8 @@
+// @ts-ignore
+// @ts-nocheck
 import React, { useState, useEffect, useRef } from "react"
 import PopularListItem from "./PopularListItem"
+import SectionTitle from "../../SectionTitle/SectionTitle"
 
 import articles from "../../../../data/articles"
 import styles from "./popular.module.scss"
@@ -7,113 +10,68 @@ import styles from "./popular.module.scss"
 type Props = {}
 
 const PopularList = (props: Props) => {
-  const [left, setLeft] = useState(0)
+  // const [left, setLeft] = useState(0)
   const ref = useRef<HTMLDivElement>(null)
   const refMainDiv = useRef<HTMLDivElement>(null)
   const [visibleDiv, setVisibleDiv] = useState(true)
+  const [top, setTop] = useState()
+  const [left, setLeft] = useState()
+
+  function preventScroll(e) {
+    console.log("prevent")
+    e.preventDefault()
+    e.stopPropagation()
+
+    return false
+  }
 
   const scrollHandler = (e: any) => {
-    // let target = e.target
-
-    // while (target.scrollWidth <= target.clientWidth) {
-    //   if (target == document.body) {
-    //     console.log("1")
-    //     setVisibleDiv(false)
-    //     break
-    //   }
-    //   target = target.parentElement
-    // }
-    // if (target != document.body) {
-    //   if (target.scrollLeft <= 0 && e.deltaX < 0) {
-    //     console.log("start")
-    //     e.preventDefault()
-    //   } else if (
-    //     target.clientWidth + target.scrollWidth >= target.scrollWidth &&
-    //     e.deltaX > 0
-    //   ) {
-    //     console.log("end")
-    //     e.preventDefault()
-    //   }
-    // }
-    // console.log(ref.current!.offsetHeight, "offsetHeight")
-    // console.log(ref.current!.clientHeight, "clientHeight")
-    // console.log(ref.current!.scrollLeft, "scrollHeight")
-    // console.log(ref.current!.offsetLeft, "offsetLeft")
-    // ref.current!.style.position = "relative"
-    // ref.current!.style.left = `-${left}px`
-
-    // console.log(refMainDiv.current!.scrollWidth, "scrollWidth")
-
-    // if (refMainDiv.current!.scrollWidth === 1000) {
-    //   console.log("1000")
-    //   ref.current!.style.position = "relative"
-    //   ref.current!.style.left = "0px"
-    //   setLeft(0)
-    // } else if (e.deltaY < 0 && left >= -100) {
-    //   console.log("-")
-    //   console.log(left)
-    //   setLeft(left - 100)
-    //   ref.current!.style.position = "relative"
-    //   ref.current!.style.left = `-${left + 100}px`
-    // } else if (e.deltaY > 0 && refMainDiv.current!.scrollWidth !== 1000) {
-    //   console.log("+")
-    //   console.log(left)
-    //   setLeft((prev) => prev + 100)
-    //   ref.current!.style.position = "relative"
-    //   ref.current!.style.left = `-${left}px`
-    // }
-
+    // document.body.addEventListener("scroll", (e: any) => e.preventDefault())
     const d = document.querySelector("#popular")
-
     if (e.deltaY > 0) d!.scrollLeft += 100
     else d!.scrollLeft -= 100
+  }
 
-    // if (e.deltaY > 0 && refMainDiv.current!.scrollWidth !== 1000) {
-    //   console.log("+")
-    //   console.log(left)
-    //   setLeft(left + 100)
-    //   ref.current!.style.position = "relative"
-    //   ref.current!.style.left = `-${left}px`
-    // } else if (e.deltaY < 0 && left >= -100) {
-    //   console.log("-")
-    //   console.log(left)
-    //   setLeft(left - 100)
-    //   ref.current!.style.position = "relative"
-    //   ref.current!.style.left = `-${left + 100}px`
+  const enterHandler = () => {
+    window.addEventListener("wheel", preventScroll, { passive: false })
+    // setTop(window.pageYOffset || document.documentElement.scrollTop)
+    // setLeft(window.pageXOffset || document.documentElement.scrollLeft)
+
+    // window.onscroll = function (e) {
+    //   console.log(e)
+    //   e.preventDefault()
     // }
+    // window.addEventListener("scroll", (e) => e.preventDefault())
   }
 
-  useEffect(() => {
-    console.log(left)
-  }, [left])
-
-  const mouseEnterHandler = (e: any) => {
-    // document.body.style.overflow = "hidden"
-    // document.body.style.position = "fixed"
+  const leaveHandler = () => {
+    window.removeEventListener("wheel", preventScroll)
+    // window.removeEventListener("scroll", enterHandler)
+    // window.onscroll = function () {}
   }
 
-  const mouseOutHandler = (e: any) => {
-    if (e.relatedTarget !== "popular_items") {
-      //   document.body.style.overflow = "auto"
-      //   document.body.style.position = "relative"
-    }
-  }
+  // useEffect(() => {}, [top, left])
 
   return (
-    <div
-      id="popular"
-      className={styles.popular}
-      onWheel={scrollHandler}
-      onMouseMove={mouseEnterHandler}
-      onMouseOut={mouseOutHandler}
-      ref={refMainDiv}
-    >
-      <div ref={ref} className={styles.popular_items}>
-        {articles.slice(0, 15).map((item) => {
-          return <PopularListItem item={item} key={item.id} />
-        })}
+    <>
+      <div className={styles.section_container}>
+        <SectionTitle title="Popular" width="100" />
+        <div
+          id="popular"
+          className={styles.popular}
+          onWheel={scrollHandler}
+          onMouseEnter={enterHandler}
+          onMouseLeave={leaveHandler}
+          ref={refMainDiv}
+        >
+          <div ref={ref} className={styles.popular_items}>
+            {articles.slice(0, 15).map((item) => {
+              return <PopularListItem item={item} key={item.id} />
+            })}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
