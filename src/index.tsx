1c8df1472168083cc996daf79c69551f3781a812
Cakctus <cakctusinc@gmail.com>
2022-12-31 15:25:26 +0200

    31/12

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
-import "./assets/scss/index.scss"
 import App from "./containers/App/App"
+import "./assets/scss/index.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
